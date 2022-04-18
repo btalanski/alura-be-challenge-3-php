18aea3cbf090bc31778a8af521a933222054307c
Bruno Talanski <brn.talanski@gmail.com>
2022-04-18 17:53:39 -0300

    Remove unnused code

--- app/src/Controller/DashboardController.php
+++ app/src/Controller/DashboardController.php
@@ -8,12 +8,9 @@ use App\Service\ReportFileReader;
 use App\Service\FileUploader;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\String\Slugger\SluggerInterface;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class DashboardController extends AbstractController
 {
